6d754b4437ad9499b5fb8bff1f1630eb570ad0b2
Rob Mayo <waldosax+github@gmail.com>
2021-07-23 13:24:34 -0400

    Added util to make hard-links for common script files.

--- create-shared-links.bat
+++ create-shared-links.bat
@@ -10,9 +10,11 @@ goto :eof
 :treeProcess
 rem Do whatever you want here over the files of this subdir, for example:
 for %%f in (*.*) do (
+	if exist "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%fc" del /F /Q "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%fc"
 	if exist "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%f" del /F /Q "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%f"
 	mklink /H "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%f" "%shareddir%\%curdir%%%f"
 	
+	if exist "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%fc" del /F /Q "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%fc"
 	if exist "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%f" del /F /Q "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%f"
 	mklink /H "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%f" "%shareddir%\%curdir%%%f"
 )
