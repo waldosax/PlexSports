3c5afaa65630cccede1a0a2bd3b116b85261d2ef
Rob Mayo <waldosax+github@gmail.com>
2021-08-06 03:13:50 -0400

    Fixed bug in recursion in utility.

--- create-shared-links.bat
+++ create-shared-links.bat
@@ -1,31 +1,56 @@
 @echo off
-SETLOCAL
+SETLOCAL EnableDelayedExpansion
 cd Shared
 
+set shareddir=!cd!
+REM ECHO !shareddir!
+(echo "%shareddir%" & echo.) | findstr /O . | more +1 | (set /P RESULT= & call exit /B %%RESULT%%)
+set /A shareddirLength=%ERRORLEVEL%-5
+SET /A subdirStart=!shareddirLength!+1
 set curdir=
-set shareddir=%cd%
+
+REM ECHO %shareddirLength%
+REM ECHO %subdirStart%
+
 call :treeProcess
 goto :eof
 
 :treeProcess
-rem Do whatever you want here over the files of this subdir, for example:
+
+REM ECHO !curdir!
+REM echo Looking in %curdir%
+REM ECHO !cd!
+REM ECHO !curdir!
 for %%f in (*.*) do (
-	if exist "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%fc" del /F /Q "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%fc"
-	if exist "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%f" del /F /Q "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%f"
-	mklink /H "%~dp0PlexSportsAgent.bundle\Contents\Code\%curdir%%%f" "%shareddir%\%curdir%%%f"
+	REM ECHO %%f
+	REM ECHO !curdir!%%f
+	REM ECHO %shareddir%!curdir!%%f
+	REM ECHO !cd!\%%f
+	if not exist "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!" mkdir "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!" >nul
+	if exist "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!%%fc" del /F /Q "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!%%fc" >nul
+	if exist "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!%%f" del /F /Q "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!%%f" >nul
+	mklink /H "%~dp0PlexSportsAgent.bundle\Contents\Code\!curdir!%%f" "%shareddir%\!curdir!%%f" >nul
 	
-	if exist "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%fc" del /F /Q "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%fc"
-	if exist "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%f" del /F /Q "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%f"
-	mklink /H "%~dp0Scanners\Series\PlexSportsScanner\%curdir%%%f" "%shareddir%\%curdir%%%f"
+	if not exist "%~dp0Scanners\Series\PlexSportsScanner\!curdir!" mkdir "%~dp0Scanners\Series\PlexSportsScanner\!curdir!" >nul
+	if exist "%~dp0Scanners\Series\PlexSportsScanner\!curdir!%%fc" del /F /Q "%~dp0Scanners\Series\PlexSportsScanner\!curdir!%%fc" >nul
+	if exist "%~dp0Scanners\Series\PlexSportsScanner\!curdir!%%f" del /F /Q "%~dp0Scanners\Series\PlexSportsScanner\!curdir!%%f" >nul
+	mklink /H "%~dp0Scanners\Series\PlexSportsScanner\!curdir!%%f" "%shareddir%\!curdir!%%f" >nul
 )
 for /D %%d in (*) do (
-	set curdir=%curdir%%%d\
-    cd %%d
-    call :treeProcess
-    cd ..
+	REM ECHO --recursing %%d
+	REM ECHO --switching to !curdir!
+	REM ECHO CURRENT: !cd!
+	REM ECHO EXPECTED: %shareddir%\!curdir!
+    REM if exist %%d (
+		cd %%d
+		CALL SET curdir=!!cd:~%subdirStart%!!\
+		REM ECHO --switched to !cd!
+		call :treeProcess
+		cd ..
+	REM )
 )
 
-ENDLOCAL
-cd %~dp0
+REM ENDLOCAL
+REM cd %~dp0
 exit /b
 
